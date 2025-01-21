3d737f48be65b9982a69eebc491743836c773ef3
nicolasmarks <nicolasmarks7@hotmail.com>
2025-01-21 16:36:44 -0300

    audios semana 4 5 6

--- src/app/data/audios.ts
+++ src/app/data/audios.ts
@@ -1,12 +1,9 @@
 export const audios = [
-  'https://www.youtube.com/watch?v=FEcLC_iaPtw', // intro
-  'https://www.youtube.com/watch?v=xnP32GNSsCY', // week 1
-  'https://youtu.be/H9WOtq6zxMQ', // week 3
+  'https://youtu.be/FEcLC_iaPtw', // intro
+  'https://youtu.be/xnP32GNSsCY', // week 1
+  'https://youtu.be/H9WOtq6zxMQ', // week 2
+  'https://youtu.be/pbztsW9So-A', // week 3
   'https://youtu.be/rR5TGY6k_cg', // week 4
-  //'https://youtu.be/pbztsW9So-A', // week 4
   'https://youtu.be/LaRQoaaGHPQ', // week 5
-  //'https://youtu.be/hJKiDUmZukE', // week 5
-  'https://youtu.be/_E8c3UC6yfg', // week 6
-  //'https://youtu.be/duJBcq5fpIc', // week 6
-  'https://youtu.be/Wh5aSiggVJw'  // week 7
+  'https://youtu.be/_E8c3UC6yfg'  // week 6
 ];
